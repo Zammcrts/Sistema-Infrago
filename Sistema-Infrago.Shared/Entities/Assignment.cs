3c08c73bdb88b673f972ef0feed7bc49432ebd1d
Zammcrts <samcortes480@gmail.com>
2024-09-17 20:57:41 -0600

    entities v3

--- Sistema-Infrago.Shared/Entities/Assignment.cs
+++ Sistema-Infrago.Shared/Entities/Assignment.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -8,5 +9,22 @@ namespace Sistema_Infrago.Shared.Entities
 {
     internal class Assignment
     {
+        public int Id { get; set; }
+
+        [Required(ErrorMessage = "El campo {0} es obligatorio")] 
+        [Display(Name = "Proyecto")]
+        [MaxLength(100, ErrorMessage = "El campo {0} no puede tener mas de {1} caracteres")]
+        public Project Project { get; set; } = null!;
+
+        [Required(ErrorMessage = "El campo {0} es obligatorio")] 
+        [Display(Name = "Material")]
+        [MaxLength(100, ErrorMessage = "El campo {0} no puede tener mas de {1} caracteres")]
+        public Material Material { get; set; } = null!;
+
+        [Required(ErrorMessage = "El campo {0} es obligatorio")] 
+        [Display(Name = "Cantidad de material")]
+        public int Quantity { get; set; }
+
+
     }
 }
--- Sistema-Infrago.Shared/Entities/Stockist.cs
+++ Sistema-Infrago.Shared/Entities/Stockist.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -8,5 +9,50 @@ namespace Sistema_Infrago.Shared.Entities
 {
     internal class Stockist
     {
+        public int Id { get; set; }
+        [Required(ErrorMessage = "El campo {0} es obligatorio")] //obligatorio pertenece al public string Name
+        [Display(Name = "Provedor")]
+        [MaxLength(100, ErrorMessage = "El campo {0} no puede tener mas de {1} caracteres")]
+        public string ProviderName { get; set; } = null!;
+
+        [Required(ErrorMessage = "El campo {0} es obligatorio")] //obligatorio pertenece al public string Name
+        [Display(Name = "Teléfono")]
+        [MaxLength(10, ErrorMessage = "El campo {0} no puede tener mas de {1} caracteres")]
+        public string Phone { get; set; } = null!;
+
+        [Required(ErrorMessage = "El campo {0} es obligatorio")] //obligatorio pertenece al public string Name
+        [Display(Name = "Email")]
+        [MaxLength(100, ErrorMessage = "El campo {0} no puede tener mas de {1} caracteres")]
+        public string Email { get; set; } = null!;
+
+        [Required(ErrorMessage = "El campo {0} es obligatorio")] //obligatorio pertenece al public string Name
+        [Display(Name = "Calle")]
+        [MaxLength(100, ErrorMessage = "El campo {0} no puede tener mas de {1} caracteres")]
+        public string Street { get; set; } = null!;
+
+        [Required(ErrorMessage = "El campo {0} es obligatorio")] //obligatorio pertenece al public string Name
+        [Display(Name = "Número exterior")]
+        [MaxLength(10, ErrorMessage = "El campo {0} no puede tener mas de {1} caracteres")]
+        public string ExteriorNumber { get; set; } = null!;
+
+        [Required(ErrorMessage = "El campo {0} es obligatorio")] //obligatorio pertenece al public string Name
+        [Display(Name = "Estado")]
+        [MaxLength(100, ErrorMessage = "El campo {0} no puede tener mas de {1} caracteres")]
+        public string State { get; set; } = null!;
+
+        [Required(ErrorMessage = "El campo {0} es obligatorio")] //obligatorio pertenece al public string Name
+        [Display(Name = "Municipio")]
+        [MaxLength(100, ErrorMessage = "El campo {0} no puede tener mas de {1} caracteres")]
+        public string Township { get; set; } = null!;
+
+        [Required(ErrorMessage = "El campo {0} es obligatorio")] //obligatorio pertenece al public string Name
+        [Display(Name = "Código Postal")]
+        [MaxLength(100, ErrorMessage = "El campo {0} no puede tener mas de {1} caracteres")]
+        public string ZipCode { get; set; } = null!;
+
+        [Required(ErrorMessage = "El campo {0} es obligatorio")] //obligatorio pertenece al public string Name
+        [Display(Name = "Material")]
+        [MaxLength(100, ErrorMessage = "El campo {0} no puede tener mas de {1} caracteres")]
+        public string Material { get; set; } = null!;
     }
 }
